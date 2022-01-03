80f4ec869ffe8989bb75203e4517c385bcfc6ad5
abdullah-alkhareji <outsiderandco@gmail.com>
2022-01-03 19:56:24 +0300

    my first commet ;D

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ let isDark = false;
 
 // 1. This function will toggle the appearance from light text to dark text
 function toggleColor() {
-  const body = document.getElementsByTagName("body")[0];
-  isDark = !isDark;
-  if (isDark) {
-    body.style.color = "black";
-  } else {
-    body.style.color = "white";
-  }
+	const body = document.getElementsByTagName("body")[0];
+	isDark = !isDark;
+	if (isDark) {
+		body.style.color = "#333";
+	} else {
+		body.style.color = "#ccc";
+	}
 }
 
 // 2. This will make everything lowercase
 function beGentle() {
-  document.getElementsByTagName("body")[0].style.textTransform = "lowercase";
+	document.getElementsByTagName("body")[0].style.textTransform = "lowercase";
 }
 
 // 3. This will make everything uppercase.
@@ -21,19 +21,19 @@ function beGentle() {
 // add shout() inside the quotation marks after onclick in index.html like this.
 // <button onclick="shout()">SHOUT!</button>
 function shout() {
-  document.getElementsByTagName("body")[0].style.textTransform = "uppercase";
+	document.getElementsByTagName("body")[0].style.textTransform = "uppercase";
 }
 
 // 4. This will fetch a random name from the internet, and replace your name with it!
 async function fetchRandomName() {
-  try {
-    const response = await fetch("https://randomuser.me/api/", {
-      method: "GET",
-    });
-    const data = await response.json();
-    const name = data.results[0].name.first;
-    document.getElementsByTagName("h3")[0].innerText = name;
-  } catch (err) {
-    alert(`Couldn't make the request, ${err.message}`);
-  }
+	try {
+		const response = await fetch("https://randomuser.me/api/", {
+			method: "GET",
+		});
+		const data = await response.json();
+		const name = data.results[0].name.first;
+		document.getElementsByTagName("h3")[0].innerText = name;
+	} catch (err) {
+		alert(`Couldn't make the request, ${err.message}`);
+	}
 }
